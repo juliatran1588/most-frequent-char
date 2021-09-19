fc215bfc44cc50589cfa2caa3caa90ba90cc088b
Julia1588 <congchuadochoi@gmail.com>
2021-09-19 23:12:08 +0700

    refactor the code

--- src/CountCharacter.java
+++ src/CountCharacter.java
@@ -1,27 +1,31 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountCharacter {
-    public static Character findFirstMostFrequentCharIn (String str){
+    public static Character findFirstMostFrequentCharIn(String str) {
         if (str == null) {
             return null;
         }
-		if (str.length() == 0) {
+        if (str.length() == 0) {
             return null;
         }
-        Character maxAppear = null;
-        int maxCount = 0;
-
-        for(int i = 0; i < str.length(); i++) {
-            char x = str.charAt(i);
-            int countX = 0;
-
-            for(int j = 0; j < str.length(); j++) {
-                if(x == str.charAt(j)) {
-                    countX++;
-                }
+        HashMap<Character, Integer> charCountMap = new HashMap<Character, Integer>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (charCountMap.containsKey(c)) {
+                charCountMap.put(c, charCountMap.get(c) + 1);
+            } else {
+                charCountMap.put(c, 1);
             }
+        }
+        int maxCount = Collections.max(charCountMap.values());
+        Character maxAppear = null;
 
-            if (countX > maxCount) {
-                maxCount = countX;
-                maxAppear = x;
+        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+            if (entry.getValue() == maxCount) {
+                maxAppear = entry.getKey();
+                break;
             }
         }
         return maxAppear;
